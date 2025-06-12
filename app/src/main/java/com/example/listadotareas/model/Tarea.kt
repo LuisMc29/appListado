c7c50ebf4b05967e6a02308e0ce1d9eb8d257303
Luis <xionshipuden@gmail.com>
2025-06-11 20:21:48 -0600

    ultima modificacion

--- app/src/main/java/com/example/listadotareas/model/Tarea.kt
+++ app/src/main/java/com/example/listadotareas/model/Tarea.kt
@@ -3,11 +3,11 @@ package com.example.listadotareas.model
 import java.util.UUID
 
 data class Tarea(
-    var id: String = UUID.randomUUID().toString(), // Mantiene la generación automática de ID
+    var id: String = UUID.randomUUID().toString(),
     var nombre: String = "",
     var descripcion: String = "",
     var fecha: String = ""
 ) {
-    constructor() : this("", "", "", "") // Constructor sin argumentos
+    constructor() : this("", "", "", "")
 }
 
